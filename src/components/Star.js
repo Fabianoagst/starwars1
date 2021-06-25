0964b2db8a263281127ccd35a70212c91a60e24a
Fabianoagst <fabiano.agst@gmail.com>
2021-06-25 15:38:11 -0300

    Projeto Star Wars quase concluído. Falta resolver os bug de filmes e personagens.

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,119 +1,124 @@
 import React, {useState, useEffect} from 'react';
 import './Star.css';
 
-
-
 function Star() {
 
   const [parametro, setParametro] = useState ("")
   const [personagem, setPersonagem] = useState ([])
-  const [fil, setFil] = useState ({})
+  const [fil, setFil] = useState ([])
   const [total, setTotal] = useState ()
   
   
-  
   useEffect(()=>{
+
+    // Função que alimenta o Banco de personagens
     async function banco(){
       let res = await fetch(`https://swapi.dev/api/people/?page=1`);
       let data = await res.json();
       const item = data.results;
       setPersonagem(item);  
     }
+
     // async function banco(){
     //   for (let i = 1; i < 83; i++) {
     //     let res = await fetch(`https://swapi.dev/api/people/${i}/`);
     //     let data = await res.json();
     //     setPersonagem(data)
-      
+    //     console.log(data)
     //   }
     // }
 
+    //Função que calcula a soma dos valores das starships
     async function valorNaves(){
-      var soma  = 0
-      var valor = 0
+      var soma  = 0;
+      var valor = 0;
       for (let i = 1; i < 64; i++) {
-        let res = await fetch(`https://swapi.dev/api/starships/${i}/`);
+        let res = await fetch(`https://swapi.dev/api/starships/${i}/`).catch();
         let data = await res.json();
-         valor = parseInt(data.cost_in_credits)
+        
+         valor = parseInt(data.cost_in_credits);
         if(Number.isInteger(valor)){
-          soma+=valor
-          
+          soma+=valor;
         }
-
       }
-        setTotal(soma)
+      
+      setTotal(soma.toFixed(2)
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.'));
       
     }
     banco();
     valorNaves();
   },[])
 
+  //Função que limpa a mensagem de erro de campo vazio
   const limparErro = () => {
     document.getElementById("campoVazio").style.display = "none";
+    document.getElementById("errou").style.display = "none"
   }
   
-  
+  //Função de pesquisar com parâmetros e condições
   const buscar = async  () => {
-    console.log (personagem)
     const person = personagem.find((e)=>e.name===parametro)
-
+    
+    //Planeta Natal
     async function casa(){
       let res = await fetch(person.homeworld);
       let data = await res.json();
       return data.name
     }
-
+    
+    //Lista de filmes
     async function filmes(){
       await person.films.forEach(async (element)=> {
         let res = await fetch(element);
         let data = await res.json();
-        console.log(data.title)
-        setFil(data.title)
-        
+        setFil(data.title);  
       });
-
     }
-
+    
     if (parametro !== ""){
+      if (person === undefined){
+        document.getElementById("errou").style.display = "block";
+      }
       if (person !== undefined){
         console.log(person);
         const planetaNatal = await casa();
-        const listaf = await filmes();
-        console.log (fil)
-          
+        await filmes(); 
 
-      document.querySelector(".resultado").innerHTML=
-        `
-          <div class="tituloDetalhes">
-            DETALHES DO PERSONAGEM
-          </div>
-          <div class= "informacoes">
-            <div class="personagem">
-              <div>
-                Nome completo: ${person.name}
+        //Inserção das informações do personagem pesquisado
+        document.querySelector(".resultado").innerHTML=
+          `
+            <div class="tituloDetalhes">
+              DETALHES DO PERSONAGEM
+            </div>
+            <div class= "informacoes">
+              <div class="personagem">
+                <div>
+                  Nome completo: ${person.name}
+                </div>
+                <div>
+                  Altura: ${person.height}
+                </div>
+                <div>
+                  Peso: ${person.mass}
+                </div>
+                <div>
+                  Planeta natal: ${planetaNatal}
+                </div>
               </div>
-              <div>
-                Altura: ${person.height}
-              </div>
-              <div>
-                Peso: ${person.mass}
-              </div>
-              <div>
-                Planeta natal: ${planetaNatal}
+              <div class="filmes">
+                <div class= "tituloFilmes">
+                  Lista dos filmes em que apareceu:
+                </div> 
+                <div class="listaFilmes">
+                ${fil}
+                </div>
               </div>
             </div>
-            <div class="filmes">
-              <div class= "tituloFilmes">
-                Lista dos filmes em que apareceu:
-              </div> 
-              <div class="listaFilmes">
-              ${listaf}
-              </div>
-            </div>
-          </div>
-        `
+          `
       }
     }else{
+      //Mensagem de campo vazio
       document.getElementById("campoVazio").style.display = "block";
     } 
   }
@@ -122,7 +127,7 @@ function Star() {
     <div className= "container">
       <div className= "header">
         <div className="titulo">
-            Star Wars
+            Star Wars Info
           </div>
           <div className="descricao">
             Para obter informações sobre um personagem do universo Star Wars, digite o nome no campo abaixo e em seguida clique em "Pesquisar".
@@ -133,8 +138,9 @@ function Star() {
           <div>
             Nome do personagem
           </div>
-          <input type="text" id="parametro" value={parametro} onInput={()=>{limparErro()}} onChange={e=>setParametro(e.target.value)} placeholder="Digite aqui"></input>
+          <input type="text" id="parametro" autocomplete="off" value={parametro} onInput={()=>{limparErro()}} onChange={e=>setParametro(e.target.value)} placeholder="Digite aqui"></input>
         <div id="campoVazio">Para pesquisar este campo não pode está vazio.</div>
+        <div id="errou">Este pesonagem não existe no universo de Star Wars.</div>
         </div>
         <div className="campoBtn">
           <button onClick={()=>{buscar()}}>Pesquisar</button>
